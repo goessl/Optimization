00f0c467320b0d3c3acb5f6ccf3c4ab82d046632
Sebastian Gössl <sebig3000@gmail.com>
2019-09-13 19:32:54 +0200

    Adapted 4 spaces tabs & fixed huge Optimizer lambda mistake
    Lambdas used all inputs/outputs & not the batches/stochastic ones.
    Removed lambda brackets.

--- Optimizable.java
+++ Optimizable.java
@@ -32,41 +32,43 @@ package optimization;
  * Interface used for mathematical optimizable classes.
  * 
  * @author Sebastian Gössl
- * @version 1.0 3.3.2019
+ * @version 1.1 13.9.2019
  * @param <T> Input & output type
  */
 public interface Optimizable<T> {
-  
-  /**
-   * Returns all parameters arranged to an array.
-   * 
-   * @return parameters arranged to an array
-   */
-  public double[] getParameters();
-  
-  /**
-   * Replaces all parameters with the given ones.
-   * 
-   * @param params new parameters
-   */
-  public void setParameters(double[] params);
-  
-  
-  /**
-   * Returns the error of this objects outputs compared to the given outputs.
-   * 
-   * @param input input
-   * @param output output
-   * @return error of this objects outputs compared to the given outputs
-   */
-  public double cost(T[] input, T[] output);
-  
-  /**
-   * Returns the derivative of the cost with respect to every parameter.
-   * 
-   * @param input input
-   * @param output wanted output
-   * @return derivative of the cost with respect to every parameter
-   */
-  public double[] costPrime(T[] input, T[] output);
+    
+    /**
+     * Returns all parameters arranged to an array.
+     * 
+     * @return parameters arranged to an array
+     */
+    public double[] getParameters();
+    
+    /**
+     * Replaces all parameters with the given ones.
+     * 
+     * @param params new parameters
+     */
+    public void setParameters(double[] params);
+    
+    
+    
+    /**
+     * Returns the error of this objects outputs compared to the given
+     * outputs.
+     * 
+     * @param input input
+     * @param output output
+     * @return error of this objects outputs compared to the given outputs
+     */
+    public double cost(T[] input, T[] output);
+    
+    /**
+     * Returns the derivative of the cost with respect to every parameter.
+     * 
+     * @param input input
+     * @param output wanted output
+     * @return derivative of the cost with respect to every parameter
+     */
+    public double[] costPrime(T[] input, T[] output);
 }
